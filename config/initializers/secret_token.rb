c374cd8a5240df5173806cb938afeb91215d2728
Guilherme Araujo <gui2811@gmail.com>
2013-04-12 00:15:18 -0300

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Eusouhacker::Application.config.secret_token = '400666a3ceb9a4ab9d1c97f057ea111d3fadd238e68eb0d3e9faafa97981267ecffe081151f2a92ffb1e6a4f124b0def61ae5be2f19a8d3acaac4ddc7756e879'
